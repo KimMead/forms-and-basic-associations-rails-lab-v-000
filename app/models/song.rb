5aaf1721fe7a5049f95ce37178fc4254f08eb6b2
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-03-10 16:45:22 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,13 +4,13 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
 
-  def artist_name 
-    self.artist.name 
-  end 
+  def artist_name
+    self.artist.name
+  end
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
-  end 
+  end
 
-  
+
 end
